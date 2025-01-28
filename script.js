70a95a085bf04d5138354e50b5d155f4ba2e0450
Adriano Bispo <drianvini10@gmail,com>
2025-01-28 15:33:06 -0300

    Código refatorado. Função 'alterarContexto()' adicionada ao projeto.

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 
 focoBt.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'foco');
-  imagemflutuante.setAttribute('src', './imagens/foco.png'); 
+  alterarContexto('foco');
 });
 
 curtoBt.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-curto');
-  imagemflutuante.setAttribute('src', './imagens/descanso-curto.png'); 
+  alterarContexto('descanso-curto');
 });
 
 longoBt.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-longo');
-  imagemflutuante.setAttribute('src', './imagens/descanso-longo.png'); 
+  alterarContexto('descanso-longo');
 });
+
+function alterarContexto(contexto) {
+  html.setAttribute('data-contexto', contexto);
+  imagemflutuante.setAttribute('src', `./imagens/${contexto}.png`);
+}
