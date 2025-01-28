941724038e340334d6d950d3074b5b4cb681f9ae
Adriano Bispo <drianvini10@gmail,com>
2025-01-28 15:28:03 -0300

    As imagens flutuantes agora estão sendo atualizadas através do evento de clique

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const html = document.querySelector('html');
+const imagemflutuante = document.querySelector('.app__image');
+
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 
 focoBt.addEventListener('click', () => {
   html.setAttribute('data-contexto', 'foco');
+  imagemflutuante.setAttribute('src', './imagens/foco.png'); 
 });
 
 curtoBt.addEventListener('click', () => {
   html.setAttribute('data-contexto', 'descanso-curto');
+  imagemflutuante.setAttribute('src', './imagens/descanso-curto.png'); 
 });
 
-longoBt.addEventListener('click', () => {    
+longoBt.addEventListener('click', () => {
   html.setAttribute('data-contexto', 'descanso-longo');
+  imagemflutuante.setAttribute('src', './imagens/descanso-longo.png'); 
 });
-
